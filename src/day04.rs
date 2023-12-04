7427fba24301db1b4307e7b5dbc79a09c575c6bf
Mark Thebridge <mthebridge@microsoft.com>
2023-12-04 12:16:34 +0000

    Day4 pt2

--- src/day04.rs
+++ src/day04.rs
@@ -12,30 +12,55 @@ fn parse_set(line: &str) -> NumberSet {
         .collect()
 }
 
+#[derive(Debug, Clone)]
+struct Card {
+    id: u8,
+    matches: u8,
+}
+
+impl Card {
+    fn part1_score(&self) -> u32 {
+        // score is 0 for no matches, 2^(n-1) for 1 or more.
+        if self.matches == 0 {
+            0
+        } else {
+            2u32.pow(self.matches as u32 - 1)
+        }
+    }
+}
+
 pub fn run(input_path: String) {
     let input = std::fs::read_to_string(input_path).unwrap();
+    let num_games = input.lines().count();
 
-    let match_scores = input.lines().map(|line| {
-        let (game, numbers) = line.split_once(": ").unwrap();
-        let _game_num = game
-            .split_once(' ')
-            .unwrap()
-            .1
-            .trim()
-            .parse::<u8>()
-            .unwrap();
+    let cards = input.lines().enumerate().map(|(idx, line)| {
+        let (_, numbers) = line.split_once(": ").unwrap();
         let mut numbersets = numbers.split(" | ");
         let winners = parse_set(numbersets.next().unwrap());
         let this_card = parse_set(numbersets.next().unwrap());
-        let match_count = winners.intersection(&this_card).count() as u32;
-        // score is 0 for no matches, 2^(n-1) for 1 or more.
-        if match_count == 0 {
-            0
-        } else {
-            2u32.pow(match_count - 1)
+        let match_count = winners.intersection(&this_card).count() as u8;
+        Card {
+            id: idx as u8 + 1,
+            matches: match_count,
         }
     });
-    println!("Part 1: {}", match_scores.sum::<u32>());
-    let part2 = 0;
-    println!("Part 2: {}", part2);
+    println!(
+        "Part 1: {}",
+        cards.clone().map(|card| card.part1_score()).sum::<u32>()
+    );
+
+    let mut card_list: Vec<Card> = cards.collect();
+    card_list.sort_by_key(|c| c.id);
+    let mut card_copies = vec![1; num_games];
+
+    for card in &card_list {
+        let this_copies = card_copies[card.id as usize - 1];
+        for i in 0..card.matches {
+            // Add a copy of the next card up.
+            let next_id = (i + card.id) as usize;
+            card_copies[next_id] += this_copies;
+        }
+    }
+
+    println!("Part 2: {}", card_copies.iter().sum::<u32>());
 }
