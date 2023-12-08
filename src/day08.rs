67b22fc4a476e317873b931aca45389854f4ddc8
Mark Thebridge <mthebridge@microsoft.com>
2023-12-08 03:44:44 +0000

    Tidy up

--- src/day08.rs
+++ src/day08.rs
@@ -1,5 +1,10 @@
-// Template.
-// Copy to daynum.rs, and uncomment relevant lins in main to add
+// Another "optimize perf for part 2".  Part 1 took me 10 minutes; part 2
+// over an hour...
+// There's a lot of assumptions here, namely that the answer is
+// a multiple of the number of steps needed to reach a target for each gjost, and that
+// they all cycle nicely in sync with the directions, neither of which I proved here.
+// Thanks to my colleagues who sahred that insight and helped me find this solution and get
+// over the line!
 
 use std::collections::HashMap;
 
@@ -19,48 +24,67 @@ impl Dir {
     }
 }
 
-fn solve<I: IntoIterator<Item = Dir> + Clone>(dirs: I, nodes: &HashMap<String, (String, String)>, part2: bool) -> u64
-    where <I as IntoIterator>::IntoIter: Clone {
-    let start_nodes = if part2 {
-        nodes.keys().filter(|k| k.ends_with('A')).map(|k| k.as_str()).collect::<Vec<_>>()
+fn solve<I: IntoIterator<Item = Dir> + Clone>(
+    dirs: I,
+    nodes: &HashMap<String, (String, String)>,
+    part2: bool,
+) -> u64
+where
+    <I as IntoIterator>::IntoIter: Clone,
+{
+    // Part 1 wants route from AAA -> ZZZ
+    // Part 2 wants routes from all ??A to any ??Z
+    let (start_nodes, targets) = if part2 {
+        (
+            nodes
+                .keys()
+                .filter(|k| k.ends_with('A'))
+                .map(|k| k.as_str())
+                .collect::<Vec<_>>(),
+            nodes
+                .keys()
+                .filter(|k| k.ends_with('Z'))
+                .map(|k| k.as_str())
+                .collect::<Vec<_>>(),
+        )
     } else {
-        vec!["AAA"]
+        (vec!["AAA"], vec!["ZZZ"])
     };
-    let targets = if part2 {
-        nodes.keys().filter(|k| k.ends_with('Z')).map(|k| k.as_str()).collect::<Vec<_>>()
-    } else {
-        vec!["ZZZ"]
-    };
-    // let dir_count = dirs.clone().into_iter().count() as u64;
 
-    // For each source, get the time to reach a target.
-    let steps = start_nodes.iter().map(|&start| {
-        let mut steps = 0u64;
-        let mut current = start;
-        for dir in dirs.clone().into_iter().cycle() {
-            steps += 1;
+    // For each source, count the steps to reach a target.
+    let steps = start_nodes
+        .iter()
+        .map(|&start| {
+            let mut steps = 0u64;
+            let mut current = start;
+            for dir in dirs.clone().into_iter().cycle() {
+                steps += 1;
 
-            let entry = nodes.get(current).unwrap();
-            let next = match dir {
+                let entry = nodes.get(current).unwrap();
+                let next = match dir {
                     Dir::Left => entry.0.as_str(),
                     Dir::Right => entry.1.as_str(),
-            };
+                };
 
-            if targets.iter().find(|&&target| target == next).is_some() {
-                break;
+                if targets.iter().find(|&&target| target == next).is_some() {
+                    break;
+                }
+
+                current = next;
             }
+            steps
+        })
+        .collect::<Vec<_>>();
 
-            current = next;
-        }
-        steps
-    }).collect::<Vec<_>>();
-
-    dbg!(&steps);
-    // For
-    steps.into_iter().fold(1, |total, next| {
-        num::integer::lcm(total, next)
-    })
-
+    // We want the minimum steps for all the targets to be reached at once.
+    // It turns out with our inputs this is the lowest common multiple of
+    // the individual scores, implying that they all form cycles in phase with
+    // the direction list...
+    // if we were doing this properly we'd need to keep track of the postion modulo
+    // length of direction lists, and do some modular arithmetic fun.
+    steps
+        .into_iter()
+        .fold(1, |total, next| num::integer::lcm(total, next))
 }
 
 pub fn run(input_path: String) {
@@ -68,11 +92,11 @@ pub fn run(input_path: String) {
     let (dir_input, map_input) = input.split_once("\n\n").unwrap();
     let dirs = dir_input.chars().map(Dir::from_char);
 
+    // Regex cos life's too short.
     let map_pattern = regex::Regex::new(r#"([A-Z1-9]+) = \(([A-Z1-9]+), ([A-Z1-9]+)\)"#).unwrap();
     let nodes = map_input
         .lines()
         .map(|l| {
-            // dbg!(&map_pattern, &l);
             let matches = map_pattern.captures(l).unwrap();
             let src = matches.get(1).unwrap().as_str();
             let left = matches.get(2).unwrap().as_str();
@@ -81,7 +105,6 @@ pub fn run(input_path: String) {
         })
         .collect::<HashMap<_, _>>();
 
-    // dbg!(&nodes);
     let part1 = solve(dirs.clone(), &nodes, false);
     println!("Part 1: {}", part1);
 
