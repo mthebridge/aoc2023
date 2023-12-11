45c3744e2064d844a462195beb0bae7c0266ab9e
Mark Thebridge <mthebridge@microsoft.com>
2023-12-11 22:31:32 +0000

    Tidy

--- src/day12.rs
+++ src/day12.rs
@@ -1,29 +1,38 @@
-// Template.
-// Copy to daynum.rs, and uncomment relevant lines in main to add
+// Part 1 took ages because I missed some key edge cases that only showed up in the real inoput.
+// Sort of brute force, although with more logic to spot when we reach an invalid poisiton than
+//perhaps we need.
+//
+// Was worried about part2 until I saw some rumblings about memoization on reddit, and indeed
+// throwing in a cache brings the runtime down to a fraction of a second, even with
+// reallocating all the time.
 
 use std::collections::HashMap;
 
+// Cache results for performance!
 type Cache = HashMap<(Vec<char>, Vec<u8>), usize>;
 
-// Work out how many possible runs we could have givemn this input.
-fn calculate_possibles(row: &[char], lengths: &[u8], cache: &mut Cache) -> usize
-{
-
-    let (row_vec, length_vec) =  (row.to_vec(), lengths.to_vec())
-;    if let Some(res) = cache.get(&(row_vec, length_vec)) {
-        return *res
+// Work out how many possible runs we could have given this input.
+// We look at the next spring, and consider all possible positions that could start given the
+// other constraints; then recurse from the point after that with the remaining springs.
+// The cache is essential for part 2 to complete in reasonable time, as we can end up
+// with billions of possibilities.
+fn calculate_possibles(row: &[char], lengths: &[u8], cache: &mut Cache) -> usize {
+    let (row_vec, length_vec) = (row.to_vec(), lengths.to_vec());
+    if let Some(res) = cache.get(&(row_vec, length_vec)) {
+        return *res;
     }
     // Check we have space for all the remaining runs.  Need all their sizes,
-    // + 1 for each run after this.
+    // + 1 for each run after this for the space.
     let space_needed = lengths.iter().sum::<u8>() as usize + lengths.len() - 1;
 
+    // Return count.
     let mut possibles = 0;
-    let this_run_len = lengths[0] as usize;
 
-    // We can potentially start a run from anywhere here on as long as we have enough space.
+    // We can potentially start a run from anywhere here onward as long as we have enough space.
+    let this_run_len = lengths[0] as usize;
     for this_run_start in 0..=row.len() - space_needed {
-        // If the previous character was a fixed #, we can't start here (or anywhere after this!)
-        // Break out now.
+        // If the previous character was a fixed #, we can't start the run here
+        // (or anywhere after this!). Break out now.
         if this_run_start > 0 && row[this_run_start - 1] == '#' {
             break;
         }
@@ -51,7 +60,7 @@ fn calculate_possibles(row: &[char], lengths: &[u8], cache: &mut Cache) -> usize
                 possibles += calculate_possibles(&row[new_run_start..], &lengths[1..], cache)
             }
         }
-    };
+    }
 
     cache.insert((row.to_vec(), lengths.to_vec()), possibles);
     possibles
@@ -60,6 +69,7 @@ fn calculate_possibles(row: &[char], lengths: &[u8], cache: &mut Cache) -> usize
 pub fn run(input_path: String) {
     let input = std::fs::read_to_string(input_path).unwrap();
     let records = input.lines().map(|line| {
+        // Parse inputs. We want a set of chars, and a set of integers.
         let (pattern, lengths) = line.split_once(' ').unwrap();
         (
             pattern.chars().collect::<Vec<_>>(),
@@ -71,13 +81,11 @@ pub fn run(input_path: String) {
     });
 
     let mut cache: Cache = HashMap::new();
-
     let mut part1 = 0;
-    for (row, lengths) in records.clone() {
-        part1 +=  calculate_possibles(&row, &lengths, &mut cache);
-        // println!("{} {:?}: {}", row.iter().collect::<String>(), &lengths, res);
-    };
 
+    for (row, lengths) in records.clone() {
+        part1 += calculate_possibles(&row, &lengths, &mut cache);
+    }
 
     println!("Part 1: {}", part1);
 
@@ -85,15 +93,13 @@ pub fn run(input_path: String) {
     for (row, lengths) in records {
         let mut full_row = row.clone();
         let mut full_lengths = lengths.clone();
-        // Add 4 copies.
+        // Add 4 copies of the data, with the patterns separated by another ?.
         for _ in 0..4 {
             full_row.push('?');
             full_row.append(&mut row.clone());
             full_lengths.append(&mut lengths.clone());
         }
-        let res = calculate_possibles(&full_row, &full_lengths, &mut cache);
-        // println!("Entry {i} has {res} options");
-        part2 += res;
+        part2 += calculate_possibles(&full_row, &full_lengths, &mut cache);
     }
 
     println!("Part 2: {}", part2);
