ce9c19c94325814e6672a12ff62e3138fce7f6d4
Mark Thebridge <mthebridge@microsoft.com>
2023-12-14 17:48:57 +0000

    Switch to mutable state

--- src/day14.rs
+++ src/day14.rs
@@ -1,10 +1,11 @@
 // Got there in the end.
 // Brute force too slow, but as per yesterday go for cacheing and then
 // assume that the cycle eventually stabilises, which it does.
-// Lots of probably unnecessary reallocating into vectors.
 //
-// Lots of silly mistakes too today: first nto reading the questiona dn going NESW instead of NWSE;
+// Lots of silly mistakes too today: first nto reading the question and going NESW instead of NWSE;
 // then not being able to get the modular arithemtic right to extrapolate forward to a billion rows...
+//
+// Refactored to switch to mutate the grid when we tilt it - this speeds up a lot.
 use std::collections::HashMap;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -17,23 +18,21 @@ enum Direction {
 
 // Tilt the grid left or right.
 // The grid is columnar, so for the rows, transpose the grid before and after.
-fn tilt_grid(grid: Vec<Vec<char>>, dir: Direction) -> Vec<Vec<char>> {
+fn tilt_grid(grid: &mut Vec<Vec<char>>, dir: Direction) {
     match dir {
-        Direction::N => grid.iter().map(|col| tilt_line(col, false)).collect(),
-        Direction::E => transpose(
-            transpose(grid)
-                .iter()
-                .map(|row| tilt_line(row, true))
-                .collect(),
-        ),
-        Direction::S => grid.iter().map(|col| tilt_line(col, true)).collect(),
-        Direction::W => transpose(
-            transpose(grid)
-                .iter()
-                .map(|row| tilt_line(row, false))
-                .collect(),
-        ),
-    }
+        Direction::N => grid.iter_mut().for_each(|col| tilt_line(col, false)),
+        Direction::E => {
+            *grid = transpose(grid);
+            grid.iter_mut().for_each(|row| tilt_line(row, true));
+            *grid = transpose(grid);
+        }
+        Direction::S => grid.iter_mut().for_each(|col| tilt_line(col, true)),
+        Direction::W => {
+            *grid = transpose(grid);
+            grid.iter_mut().for_each(|row| tilt_line(row, false));
+            *grid = transpose(grid);
+        }
+    };
 }
 
 // Helper for debugging, not used in solution.
@@ -43,32 +42,31 @@ fn print_grid(grid: &[Vec<char>]) {
         for x in 0..grid.len() {
             print!("{}", grid[x][y]);
         }
-        println!("");
+        println!();
     }
 }
 
 //Move the rocks ona  single line tilted towards either end.
-fn tilt_line(line: &[char], rev: bool) -> Vec<char> {
+fn tilt_line(line: &mut [char], rev: bool) {
     // Iterate down the line.  For each rock `O`, we want to work out where it
     // would end up rolled towards the end, which is:
     // - the first space after the last fixed rock (#)
     // - plus one for each rock between the fixed and the last
     let mut last_fixed = 0;
     let mut rocks_this_stretch = 0;
-    // Annoyingly have to colelct here to keep the types the same in both branches.
-    let line: Vec<_> = if rev {
-        line.iter().cloned().rev().collect()
-    } else {
-        line.to_vec()
-    };
-    let mut new_line: Vec<_> = line.clone();
-    for (i, c) in line.iter().enumerate() {
-        match c {
+    // Annoyingly have to collect here to keep the types the same in both branches.
+    if rev {
+        line.reverse()
+    }
+
+    for i in 0..line.len() {
+        let this_char = line[i];
+        match this_char {
             'O' => {
                 let new_pos = last_fixed + rocks_this_stretch;
                 if new_pos != i {
-                    new_line[new_pos] = c;
-                    new_line[i] = '.';
+                    line[new_pos] = this_char;
+                    line[i] = '.';
                 }
                 rocks_this_stretch += 1;
             }
@@ -82,9 +80,8 @@ fn tilt_line(line: &[char], rev: bool) -> Vec<char> {
     }
 
     if rev {
-        new_line.reverse();
+        line.reverse();
     }
-    new_line
 }
 
 // Calculate the northbound load, column by column
@@ -102,7 +99,7 @@ fn calculate_load(grid: &[Vec<char>]) -> usize {
 }
 
 // helper to siwtch array of columns to array of rows, and vice versa.
-fn transpose<T: Copy>(grid: Vec<Vec<T>>) -> Vec<Vec<T>> {
+fn transpose<T: Copy>(grid: &[Vec<T>]) -> Vec<Vec<T>> {
     (0..grid[0].len())
         .map(|i| (0..grid.len()).map(|j| grid[j][i]).collect::<Vec<_>>())
         .collect::<Vec<_>>()
@@ -122,8 +119,8 @@ pub fn run(input_path: String) {
             }
         }
     }
-    let part1_grid = tilt_grid(columns.clone(), Direction::N);
-    let part1 = calculate_load(&part1_grid);
+    tilt_grid(&mut columns, Direction::N);
+    let part1 = calculate_load(&columns);
     println!("Part 1: {}", part1);
 
     let mut new_grid = columns;
@@ -150,10 +147,10 @@ pub fn run(input_path: String) {
         cache.insert(new_grid.clone(), i);
 
         // Tilt the grid
-        new_grid = tilt_grid(new_grid, Direction::N);
-        new_grid = tilt_grid(new_grid, Direction::W);
-        new_grid = tilt_grid(new_grid, Direction::S);
-        new_grid = tilt_grid(new_grid, Direction::E);
+        tilt_grid(&mut new_grid, Direction::N);
+        tilt_grid(&mut new_grid, Direction::W);
+        tilt_grid(&mut new_grid, Direction::S);
+        tilt_grid(&mut new_grid, Direction::E);
     }
     let part2 = calculate_load(&new_grid);
     println!("Part 2: {}", part2);
