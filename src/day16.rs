6b3165a3c6a6f84a236f3feccd44e475be1b7f60
Mark Thebridge <mark.thebridge@gmail.com>
2023-12-16 16:24:34 +0000

    Slight simplification and speedup

--- src/day16.rs
+++ src/day16.rs
@@ -25,66 +25,36 @@ fn get_neighbour(
     dir: Direction,
     max_x: usize,
     max_y: usize,
-) -> Option<(usize, usize)> {
+) -> impl Iterator<Item = (Direction, (usize, usize))> {
     match dir {
-        Direction::N => {
-            if y > 0 {
-                Some((x, y - 1))
-            } else {
-                None
-            }
-        }
-        Direction::W => {
-            if x > 0 {
-                Some((x - 1, y))
-            } else {
-                None
-            }
-        }
-        Direction::S => {
-            if y < max_y - 1 {
-                Some((x, y + 1))
-            } else {
-                None
-            }
-        }
-        Direction::E => {
-            if x < max_x - 1 {
-                Some((x + 1, y))
-            } else {
-                None
-            }
-        }
+        Direction::N if y > 0 => Some((dir, (x, y - 1))),
+        Direction::W if x > 0 => Some((dir, (x - 1, y))),
+        Direction::S if y < max_y - 1 => Some((dir, (x, y + 1))),
+        Direction::E if x < max_x - 1 => Some((dir, (x + 1, y))),
+        _ => None,
     }
+    .into_iter()
 }
 
-// Get the cells a beam will hit next.  Returns 0, 1, or 2 beams (in the case of splitters).
-fn get_next_cells(
+// Get the cells a beam will hit next, and add them to the list.
+fn extend_single_beam(
     grid: &Grid,
+    beams: &mut Vec<(Direction, (usize, usize))>,
     entry_dir: Direction,
     cell: (usize, usize),
-) -> Vec<(Direction, (usize, usize))> {
+) {
     let (x, y) = cell;
     let this = grid[y][x];
     let max_x = grid[0].len();
     let max_y = grid.len();
     match this {
         // Keep going if empty or a parallel splitter
-        '.' => get_neighbour(x, y, entry_dir, max_x, max_y)
-            .map(|pos| (entry_dir, pos))
-            .into_iter()
-            .collect(),
+        '.' => beams.extend(get_neighbour(x, y, entry_dir, max_x, max_y)),
         '|' if entry_dir == Direction::N || entry_dir == Direction::S => {
-            get_neighbour(x, y, entry_dir, max_x, max_y)
-                .map(|pos| (entry_dir, pos))
-                .into_iter()
-                .collect()
+            beams.extend(get_neighbour(x, y, entry_dir, max_x, max_y));
         }
         '-' if entry_dir == Direction::E || entry_dir == Direction::W => {
-            get_neighbour(x, y, entry_dir, max_x, max_y)
-                .map(|pos| (entry_dir, pos))
-                .into_iter()
-                .collect()
+            beams.extend(get_neighbour(x, y, entry_dir, max_x, max_y))
         }
         // Bend
         '/' => {
@@ -94,10 +64,7 @@ fn get_next_cells(
                 Direction::S => Direction::W,
                 Direction::W => Direction::S,
             };
-            get_neighbour(x, y, new_dir, max_x, max_y)
-                .map(|pos| (new_dir, pos))
-                .into_iter()
-                .collect()
+            beams.extend(get_neighbour(x, y, new_dir, max_x, max_y))
         }
         '\\' => {
             let new_dir = match entry_dir {
@@ -106,22 +73,27 @@ fn get_next_cells(
                 Direction::S => Direction::E,
                 Direction::W => Direction::N,
             };
-            get_neighbour(x, y, new_dir, max_x, max_y)
-                .map(|pos| (new_dir, pos))
-                .into_iter()
-                .collect()
+            beams.extend(get_neighbour(x, y, new_dir, max_x, max_y))
         }
         // Splits.
-        '-' => get_neighbour(x, y, Direction::E, max_x, max_y)
-            .map(|pos| (Direction::E, pos))
-            .into_iter()
-            .chain(get_neighbour(x, y, Direction::W, max_x, max_y).map(|pos| (Direction::W, pos)))
-            .collect(),
-        '|' => get_neighbour(x, y, Direction::N, max_x, max_y)
-            .map(|pos| (Direction::N, pos))
-            .into_iter()
-            .chain(get_neighbour(x, y, Direction::S, max_x, max_y).map(|pos| (Direction::S, pos)))
-            .collect(),
+        '-' => beams.extend(
+            get_neighbour(x, y, Direction::E, max_x, max_y).chain(get_neighbour(
+                x,
+                y,
+                Direction::W,
+                max_x,
+                max_y,
+            )),
+        ),
+        '|' => beams.extend(
+            get_neighbour(x, y, Direction::N, max_x, max_y).chain(get_neighbour(
+                x,
+                y,
+                Direction::S,
+                max_x,
+                max_y,
+            )),
+        ),
         _ => panic!("Invalid char"),
     }
 }
@@ -132,19 +104,14 @@ fn get_next_cells(
 fn get_energize_count(grid: &Grid, start_dir: Direction, start_pos: (usize, usize)) -> usize {
     let mut beams = vec![(start_dir, start_pos)];
     let mut visited = HashSet::new();
-    while !beams.is_empty() {
-        beams = beams
-            .iter()
-            .flat_map(|(dir, pos)| {
-                // Move on if we haven't arrived at this cell from this direction
-                if visited.insert((*dir, *pos)) {
-                    get_next_cells(grid, *dir, *pos)
-                } else {
-                    vec![]
-                }
-            })
-            .collect();
+
+    while let Some((dir, pos)) = beams.pop() {
+        // Move on if we haven't arrived at this cell from this direction
+        if visited.insert((dir, pos)) {
+            extend_single_beam(grid, &mut beams, dir, pos)
+        }
     }
+
     // We want to de-deuplicate on direction, so re-combine into a HashSet that drops the direction,
     // and then take its size to get the energized cell count.
     // (We could separately be building up the two hashSets, but perf is good enough).
