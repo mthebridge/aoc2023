a5a572b468f65622129432e6236a5a0a6a205b6c
Mark Thebridge <mthebridge@microsoft.com>
2024-06-26 17:03:53 +0100

    Day 19 pt2 - incorrect

--- src/day19.rs
+++ src/day19.rs
@@ -2,7 +2,10 @@
 // Part 2 more interesting.  Brute force clearly won't work (4000^4 is way too many options).
 // However, we can analyze the rule flows, and work out where the actual important
 // boundaries are.  We can then use that to track the total number of acceptable parts.
-use std::{collections::HashMap, str::FromStr};
+use std::{
+    collections::{HashMap, VecDeque},
+    str::FromStr,
+};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum RuleTarget {
@@ -39,6 +42,15 @@ struct Part {
 }
 
 impl Part {
+    fn with_attr_value(val: u64) -> Self {
+        Part {
+            x: val,
+            a: val,
+            m: val,
+            s: val,
+        }
+    }
+
     fn get_value(&self) -> u64 {
         self.x + self.m + self.a + self.s
     }
@@ -87,6 +99,158 @@ fn is_accepted(part: &Part, rules: &HashMap<&str, Vec<Rule>>) -> bool {
         }
     }
     unreachable!("No accept or reject rule found")
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct RuleState {
+    minvals: Part,
+    maxvals: Part,
+}
+
+fn count_acceptable_parts(rules: &HashMap<&str, Vec<Rule>>, max_attr: u64) -> u64 {
+    let mut workflow = rules.get("in").unwrap().iter();
+    let first_rule = workflow.next().unwrap();
+    // Queue of rules to process.
+    let mut stack = VecDeque::new();
+    stack.push_back((
+        first_rule,
+        RuleState {
+            minvals: Part::with_attr_value(1),
+            maxvals: Part::with_attr_value(max_attr),
+        },
+    ));
+    let mut final_states: Vec<RuleState> = vec![];
+
+    while let Some((rule, state)) = stack.pop_front() {
+        let mut true_state = state.clone();
+        let mut false_state = state.clone();
+        match rule.cond {
+            Cond::None => (),
+            Cond::Less => {
+                match rule.cond_var.as_str() {
+                    // The "if-true" maximum is the smaller of the current maximum, and one above the rule limit, down to the minimum.
+                    // The "if-false" minimum is the larger of the current minimum, and the rule limit, up to the maximum.
+                    "x" => {
+                        true_state.maxvals.x = state.maxvals.x.min(rule.limit - 1).max(state.minvals.x);
+                        false_state.minvals.x = state.minvals.x.max(rule.limit).min(state.maxvals.x)
+                    }
+                    "m" => {
+                        true_state.maxvals.m = state.maxvals.m.min(rule.limit - 1).max(state.minvals.m);
+                        false_state.minvals.m = state.minvals.m.max(rule.limit).min(state.maxvals.m)
+                    }
+                    "a" => {
+                        true_state.maxvals.a = state.maxvals.a.min(rule.limit - 1).max(state.minvals.a);
+                        false_state.minvals.a = state.minvals.a.max(rule.limit).min(state.maxvals.a)
+                    }
+                    "s" => {
+                        true_state.maxvals.s = state.maxvals.s.min(rule.limit - 1).max(state.minvals.s);
+                        false_state.minvals.s = state.minvals.s.max(rule.limit).min(state.maxvals.s)
+                    }
+                    _ => panic!("bad attr"),
+                }
+            }
+            Cond::Greater => {
+                match rule.cond_var.as_str() {
+                    // The "if-true" minimum is the larger of the current minimum, and the rule limit, up to the maximum.
+                    // The "if-false" maximum is the smaller of the current maximum, and one above the rule limit, down to the minimum.
+                    "x" => {
+                        true_state.minvals.x =
+                            state.minvals.x.max(rule.limit + 1).min(state.maxvals.x);
+                        false_state.maxvals.x = state.maxvals.x.min(rule.limit).max(state.minvals.x)
+                    }
+                    "m" => {
+                        true_state.minvals.m =
+                            state.minvals.m.max(rule.limit + 1).min(state.maxvals.m);
+                        false_state.maxvals.m =
+                            state.maxvals.m.min(rule.limit).max(state.minvals.m);
+                    }
+                    "a" => {
+                        true_state.minvals.a =
+                            state.minvals.a.max(rule.limit + 1).min(state.maxvals.a);
+                        false_state.maxvals.a =
+                            state.maxvals.a.min(rule.limit).max(state.minvals.a);
+                    }
+                    "s" => {
+                        true_state.minvals.s =
+                            state.minvals.s.max(rule.limit + 1).min(state.maxvals.s);
+                        false_state.maxvals.s =
+                            state.maxvals.s.min(rule.limit).max(state.minvals.s);
+                    }
+                    _ => panic!("bad attr"),
+                };
+            }
+        };
+
+        // If there's another rule after this one, add it to the stack as the false option
+        if let Some(new_rule) = workflow.next() {
+            stack.push_back((new_rule, false_state));
+        }
+
+        // Add the count or next steps for true state
+        match &rule.if_true {
+            RuleTarget::Accept => {
+                // Valid range.  We should add this to the final states.
+                // But before we do, remove any overlaps.
+                for other in &final_states {
+                    if true_state.minvals.x <= other.maxvals.x
+                        && true_state.maxvals.x >= other.minvals.x
+                    {
+                        if true_state.minvals.x < other.minvals.x {
+                            true_state.maxvals.x = other.minvals.x;
+                        } else {
+                            true_state.minvals.x = other.maxvals.x;
+                        }
+                    }
+                    if true_state.minvals.a <= other.maxvals.a
+                        && true_state.maxvals.a >= other.minvals.a
+                    {
+                        if true_state.minvals.a < other.minvals.a {
+                            true_state.maxvals.a = other.minvals.a;
+                        } else {
+                            true_state.minvals.a = other.maxvals.a;
+                        }
+                    }
+                    if true_state.minvals.s <= other.maxvals.s
+                        && true_state.maxvals.s >= other.minvals.s
+                    {
+                        if true_state.minvals.s < other.minvals.s {
+                            true_state.maxvals.s = other.minvals.s;
+                        } else {
+                            true_state.minvals.s = other.maxvals.s;
+                        }
+                    }
+                    if true_state.minvals.m <= other.maxvals.m
+                        && true_state.maxvals.m >= other.minvals.m
+                    {
+                        if true_state.minvals.m < other.minvals.m {
+                            true_state.maxvals.m = other.minvals.m;
+                        } else {
+                            true_state.minvals.m = other.maxvals.m;
+                        };
+                    }
+                }
+                final_states.push(true_state);
+            }
+            RuleTarget::Reject => (),
+            RuleTarget::Jump(t) => {
+                workflow = rules.get(t.as_str()).unwrap().iter();
+                let new_rule = workflow.next().unwrap();
+                stack.push_back((new_rule, true_state))
+            }
+        }
+    }
+
+    // Now merge the ranges and count them.
+    dbg!(&final_states);
+    let (x, a, s, m) = final_states
+        .iter()
+        .fold((0, 0, 0, 0), |(x, a, s, m), state|{
+            ((x + 1 + state.maxvals.x - state.minvals.x),
+            (a + 1 + state.maxvals.a - state.minvals.a),
+            (s + 1 + state.maxvals.s - state.minvals.s),
+            (m + 1 + state.maxvals.m - state.minvals.m),
+        )});
+    x * a * s* m
 }
 
 pub fn run(input_path: String) {
@@ -168,6 +332,6 @@ pub fn run(input_path: String) {
         .sum();
     println!("Part 1: {}", part1);
 
-    let part2 = 0;
+    let part2 = count_acceptable_parts(&rules, 4000);
     println!("Part 2: {}", part2);
 }
