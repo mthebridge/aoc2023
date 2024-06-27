657c56144b0bcd8324585e55b11022c832ad3933
Mark Thebridge <mark.thebridge@gmail.com>
2024-06-27 08:41:43 +0100

    Day20 template

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ mod day16;
 mod day17;
 mod day18;
 mod day19;
-// mod day20;
+mod day20;
 // mod day21;
 // mod day22;
 // mod day23;
@@ -53,7 +53,7 @@ fn main() -> Result<(), String> {
         17 => day17::run(input_path),
         18 => day18::run(input_path),
         19 => day19::run(input_path),
-        // 20 => day10::run(input_path),
+        20 => day10::run(input_path),
         // 21 => day21::run(input_path),
         // 22 => day22::run(input_path),
         // 23 => day23::run(input_path),
